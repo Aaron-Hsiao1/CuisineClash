270ddac8504516759bb8cfb464467855474e25ad
Aaron-Hsiao1 <aaronh8227@gmail.com>
2024-06-20 16:02:01 -0700

    added debug.log for death

--- Assets/Scripts/DIEDIEDIEDIE.cs
+++ Assets/Scripts/DIEDIEDIEDIE.cs
@@ -3,32 +3,33 @@ using TMPro;
 
 public class DeathZone : MonoBehaviour
 {
-    public TMP_Text gameOverText; 
+	public TMP_Text gameOverText;
 
-    private void Start()
-    {
-        gameOverText = GameObject.Find("GameOverText").GetComponent<TMP_Text>();
-        if (gameOverText != null)
-        {
-            gameOverText.gameObject.SetActive(false);
-        }
-    }
+	private void Start()
+	{
+		gameOverText = GameObject.Find("GameOverText").GetComponent<TMP_Text>();
+		if (gameOverText != null)
+		{
+			gameOverText.gameObject.SetActive(false);
+		}
+	}
 
-    private void OnTriggerEnter(Collider touch)
-    {
-        if (touch.CompareTag("DeathZone"))
-        {
-            Destroy(gameObject);
-            ShowGameOverText();
-        }
-    }
+	private void OnTriggerEnter(Collider touch)
+	{
+		if (touch.CompareTag("DeathZone"))
+		{
+			Destroy(gameObject);
+			ShowGameOverText();
+		}
+	}
 
-    private void ShowGameOverText()
-    {
-        if (gameOverText != null)
-        {
-            gameOverText.gameObject.SetActive(true);
-        }
-        
-    }
+	private void ShowGameOverText()
+	{
+		if (gameOverText != null)
+		{
+			Debug.Log("Player Died");
+			gameOverText.gameObject.SetActive(true);
+		}
+
+	}
 }
