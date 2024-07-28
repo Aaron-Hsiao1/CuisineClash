6cce9f79cee5b8ca52890b8eefd86d7f725bbeb1
CatGirlASF <nnguyenbriann@gmail.com>
2024-07-28 01:05:20 -0700

    pushing when hit pillar

--- Assets/Scripts/KOTGFireSpawn.cs
+++ Assets/Scripts/KOTGFireSpawn.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class KOTGFireSpawn : MonoBehaviour
 {
+    public float launchForce = 500f;
+    private bool hit = false;
     public GameObject pillarPrefab; // Prefab of the pillar to spawn
     public GameObject fireIndicatorPrefab;
     public float moveSpeed = 2f;    // Speed at which the pillar moves upwards
@@ -15,6 +17,29 @@ public class KOTGFireSpawn : MonoBehaviour
     private Vector3 boxSpawn = new Vector3(-14, -132, -1);
 
     // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (hit == false)
+            {
+                Rigidbody rb = other.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    // Generate a random direction
+                    Vector3 randomDirection = Random.onUnitSphere;
+
+                    // Apply the launch force in the random direction
+                    rb.AddForce(randomDirection * launchForce, ForceMode.Impulse);
+                }
+                else
+                {
+                    Debug.LogError("Rigidbody component missing from the player GameObject.");
+                }
+            }
+        }
+    }
     void Start()
     {
 
