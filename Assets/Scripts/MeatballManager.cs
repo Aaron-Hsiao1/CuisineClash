872237301660cfcb6b085660bda7b8e42bf7fa5d
Aaron-Hsiao1 <76534712+Aaron-Hsiao1@users.noreply.github.com>
2024-11-05 14:05:03 -0800

    fixed host for meatball(?)

--- Assets/Scripts/MeatballManager.cs
+++ Assets/Scripts/MeatballManager.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class MeatballManager : NetworkBehaviour
 {
-    [SerializeField] private RainingMeatballManager meatballManager;
+    [SerializeField] private RainingMeatballManager rainingMeatballManager;
 
     private Rigidbody rb;
 
@@ -20,14 +20,14 @@ public class MeatballManager : NetworkBehaviour
             gameOverText.gameObject.SetActive(false);
         }*/
         rb = GetComponent<Rigidbody>();
-        meatballManager = GameObject.Find("Raining Meatball Manager").GetComponent<RainingMeatballManager>();
+        rainingMeatballManager = GameObject.Find("Raining Meatball Manager").GetComponent<RainingMeatballManager>();
     }
 
     public override void OnNetworkSpawn()
     {
         //Debug.Log("");
         //gameOverText = GameObject.Find("GameOverText").GetComponent<TMP_Text>();
-        meatballManager = GameObject.Find("Raining Meatball Manager").GetComponent<RainingMeatballManager>();
+        rainingMeatballManager = GameObject.Find("Raining Meatball Manager").GetComponent<RainingMeatballManager>();
         //Debug.Log("meatballManager != null" + meatballManager != null);
     }
 
@@ -38,37 +38,31 @@ public class MeatballManager : NetworkBehaviour
 
     private void OnTriggerEnter(Collider collision)
 	{
-		if (collision.gameObject.CompareTag("Ground") && IsServer)
-		{
-			DestroyMeatballServerRpc();
-		}
-
         if (collision.CompareTag("Player")) //enter death zone
         {
             Debug.Log("Player died");
             RemoveFromPlayerListServerRpc();
-            ShowGameOverText();
         }
+
+        if (collision.gameObject.CompareTag("Ground") && IsServer)
+		{
+			//DestroyMeatballServerRpc();
+		}
+
+        
     }
 
     [ServerRpc(RequireOwnership = false)]
     private void RemoveFromPlayerListServerRpc(ServerRpcParams serverRpcParams = default)
     {
         var clientId = serverRpcParams.Receive.SenderClientId;
-        Destroy(NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.gameObject);
-        meatballManager.EliminatePlayer(clientId);
+        Debug.Log("Sender Client ID:" + clientId);
+        Destroy(NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.gameObject); //object destroyed but stillt rying to access after host dies and then the clinet dies.
+        rainingMeatballManager.EliminatePlayer(clientId);
 
         Debug.Log("Player removed: " + clientId);
-    }
 
-    private void ShowGameOverText()
-    {
-        /*if (gameOverText != null)
-        {
-            Debug.Log("Player Died");
-            gameOverText.gameObject.SetActive(true);
-        }*/
-
+        gameObject.GetComponent<NetworkObject>().Despawn(true);
     }
 
     [ServerRpc]
