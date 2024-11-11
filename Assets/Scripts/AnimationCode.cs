2713a3e512b24f59ca3e631fb2ca3dbe2ea46a48
Aaron-Hsiao1 <aaronh8227@gmail.com>
2024-11-10 23:35:21 -0800

    animation synced, minor code changes (changed public to private for variables)

--- Assets/Scripts/AnimationCode.cs
+++ Assets/Scripts/AnimationCode.cs
@@ -6,51 +6,52 @@ using Unity.Netcode;
 
 public class AnimationCode : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    private Animator mAnimator;
-    public GameObject otherObject;
-    private bool grounded;
-    public LayerMask Ground;
-    public float playerHeight;
-    public KeyCode jumpKey = KeyCode.Space;
-    void Start()
-    {
-        mAnimator = otherObject.GetComponent<Animator>();
-    }
+	// Start is called before the first frame update
+	private Animator mAnimator;
+	public GameObject otherObject;
+	private bool grounded;
+	public LayerMask Ground;
+	public float playerHeight;
+	public KeyCode jumpKey = KeyCode.Space;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!IsLocalPlayer)
-        {
-            return;
-        }
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+	[SerializeField] private PlayerMovement playerMovement;
 
-        if (mAnimator != null)
-        {
-            if (movementDirection != Vector3.zero)
-            {
-                mAnimator.SetBool("IsMoving", true);
-            }
-            else
-            {
-                mAnimator.SetBool("IsMoving", false);
-            }
-            if (grounded)
-            {
-                if (Input.GetKey(jumpKey))
-                {
-                    mAnimator.SetBool("IsInAir", true);
-                }
-                else
-                {
-                    mAnimator.SetBool("IsInAir", false);
-                }
-            }
-        }
-    }
+	void Awake()
+	{
+		mAnimator = otherObject.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!IsLocalPlayer)
+		{
+			return;
+		}
+		grounded = playerMovement.IsGrounded();
+		Vector3 movementDirection = playerMovement.GetMoveDirection();
+
+		if (mAnimator != null)
+		{
+			if (movementDirection != Vector3.zero)
+			{
+				mAnimator.SetBool("IsMoving", true);
+			}
+			else
+			{
+				mAnimator.SetBool("IsMoving", false);
+			}
+			if (grounded)
+			{
+				if (Input.GetKey(jumpKey))
+				{
+					mAnimator.SetBool("IsInAir", true);
+				}
+				else
+				{
+					mAnimator.SetBool("IsInAir", false);
+				}
+			}
+		}
+	}
 }
