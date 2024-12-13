eed85e2722ceaeca856dc83d68b5b455c1604630
Aaron-Hsiao1 <76534712+Aaron-Hsiao1@users.noreply.github.com>
2024-12-13 14:03:36 -0800

    CHANGED TO URP BRIAN

--- Assets/Scripts/HotPotato/PushingPlayer.cs
+++ Assets/Scripts/HotPotato/PushingPlayer.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Burst.CompilerServices;
+using Unity.Netcode;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -9,29 +11,28 @@ using UnityEngine.UI;
 public class PlayerPush : MonoBehaviour
 {
     // Public variables that can be adjusted in the Unity Inspector
-    public float pushForce = 10f; // Force to push the other player
-    public float pushUpForce = 5f; // Upward force to push the other player
+    public float pushForce = 100f; // Force to push the other player
+    public float pushUpForce = 50f; // Upward force to push the other player
     public float raycastDistance = 100f; // Distance for the raycast
+
+    [SerializeField] private GameObject playerPushLocation;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
         {
-            int LayerIgnoreRaycast = LayerMask.NameToLayer("Ignore Raycast");
-            gameObject.layer = LayerIgnoreRaycast;
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            Debug.Log("B pressed");
 
-            Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 2.0f);
-
-            if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Player"))
+            if (Physics.Raycast(playerPushLocation.transform.position, playerPushLocation.transform.forward, out hit, raycastDistance) && hit.collider.CompareTag("Player"))
             {
+                Debug.Log("hit");
+                Debug.DrawRay(playerPushLocation.transform.position, playerPushLocation.transform.forward, Color.red, 2);
+#if UNITY_EDITOR
+                    //EditorGUIUtility.PingObject(hit.collider.gameObject);
+#endif
                 PushPlayer(hit.collider.gameObject);
             }
-
-            int layerDefault = LayerMask.NameToLayer("Default");
-            gameObject.layer = layerDefault;
             
         }
         
@@ -62,14 +63,36 @@ public class PlayerPush : MonoBehaviour
     // Method to apply forces to push the other player
     void PushPlayer(GameObject player)
     {
+        Debug.Log("push player called");
         Rigidbody rb = player.GetComponentInParent<Rigidbody>();
 
         if (rb != null)
         {
+            Debug.Log("rb not null adding force");
+            Debug.Log("player.transformp osition: " + player.transform.position);
+            Debug.Log("transform.position: " + transform.position);
             Vector3 pushDirection = (player.transform.position - transform.position).normalized;
-            pushDirection.y = 0; // Neutralize the vertical component
-            rb.AddForce(pushDirection * pushForce, ForceMode.Impulse);
-            rb.AddForce(Vector3.up * pushUpForce, ForceMode.Impulse);
+
+            PushPlayerClientRpc(rb.gameObject.GetComponent<NetworkObject>().NetworkObjectId, pushDirection);
+
+            //rb.AddForce(pushDirection * pushForce, ForceMode.Impulse);
+            //rb.AddForce(Vector3.up * pushUpForce, ForceMode.Impulse);
         }
     }
+
+    [ClientRpc()]
+    private void PushPlayerClientRpc(ulong networkObjectId, Vector3 pushDirection)
+    {
+        Debug.Log("push player client rpc called");
+        GameObject playerToPush = NetworkManager.Singleton.SpawnManager.SpawnedObjects[networkObjectId].gameObject;
+        Debug.Log("player top push null ? " + playerToPush == null);
+
+#if UNITY_EDITOR
+        EditorGUIUtility.PingObject(playerToPush);
+#endif
+
+        playerToPush.GetComponent<Rigidbody>().AddForce(pushDirection * pushForce, ForceMode.Impulse);
+        playerToPush.GetComponent<Rigidbody>().AddForce(Vector3.up * pushUpForce, ForceMode.Impulse);
+    }
+
 }
