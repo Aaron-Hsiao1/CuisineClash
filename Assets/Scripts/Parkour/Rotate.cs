f0d7da24df44996eb261a66936dffd01b5d02903
SeojunJaden <143832959+SeojunJaden@users.noreply.github.com>
2024-12-16 13:36:55 -0800

    rotate

---
+++ Assets/Scripts/Parkour/Rotate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    float rotateSpeed;
+    [SerializeField]
+    Vector3 rotationDirection = new Vector3();
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateSpeed*rotationDirection * Time.deltaTime);    
+    }
+}
